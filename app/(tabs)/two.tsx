38505ed9188d3af8c8798c80c07c19e670673762
Pintoe <pintodarren999@gmail.com>
2023-10-30 23:13:34 -0500

    v1.0.01
    Comments Added

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,86 +1,62 @@
+// Import necessary components and modules from React and React Native libraries
 import React, { useState, useEffect, useRef } from "react";
-
 import { StyleSheet } from "react-native";
-
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import Slider from "@react-native-community/slider";
 import { Button, Image } from "react-native-elements";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { quotes } from "../../data/quotes";
 
-const quotes = [
-  "In the midst of chaos, find your calm; it's the eye of the storm where clarity resides.",
-  "Calmness is not the absence of storms, but the ability to find peace within them.",
-  "In the waves of life, be the calm sailor steering your own ship.",
-  "Calmness isn't about avoiding turbulence, it's about navigating through it with a composed spirit.",
-  "The serenity within you is the anchor in life's unpredictable seas.",
-  "Amidst the noise, discover the power of silence within - therein lies your calm.",
-  "Calmness is the ultimate superpower; it allows you to respond rather than react.",
-  "Sometimes the most productive thing you can do is relax into the calm of the moment.",
-  "Finding tranquility within yourself is the gateway to inner strength and resilience.",
-  "Calmness is a skill—a practice that brings peace to the chaos and clarity to the confusion.",
-  "In the center of the storm, there is stillness. In the center of stillness, there is you, calm and unshaken.",
-  "Calmness isn't complacency; it's the stronghold from which you make your most thoughtful decisions.",
-  "Embrace the serenity within; it's the compass guiding you through life's tempests.",
-  "Your inner peace is a sanctuary. Guard it fiercely and return to it often.",
-  "Calmness doesn't mean a lack of passion or drive; it means channeling your energy purposefully and harmoniously.",
-  "To be calm is to have mastered the art of riding the waves without letting them disturb your inner peace.",
-  "The quieter you become, the more you can hear. In silence, find the profound calm.",
-  "Calmness is the bridge between reaction and response. It grants you the wisdom to choose.",
-  "Peace is not found by escaping the storm but by finding calm within it.",
-  "Your tranquility is your superpower; it radiates resilience in the face of adversity.",
-];
-
+// Select a random index from the 'quotes' array
 let i = Math.floor(Math.random() * quotes.length);
-let z = Math.floor(Math.random() * quotes.length);
 
+// Set the image source for the logo
 let logoSource = require("../../assets/images/Tranquility.png");
 
+// Define the main functional component for the TabTwoScreen
 export default function TabTwoScreen() {
+  // State to handle the value for slider
   const [value, setValue] = useState(2.5);
 
+  // Function to store the value in AsyncStorage
   const storeData = async (value) => {
     try {
       await AsyncStorage.setItem("actionTime", value);
     } catch (e) {
-      // saving error
+      // Error handling if saving fails
     }
   };
-
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("actionTime");
-      console.log(value);
-      if (value !== null) {
-        // value previously stored
-      }
-    } catch (e) {
-      // error reading value
-    }
-  };
-
+  // Store the current slider value in AsyncStorage
   storeData((value * 1000).toString());
 
   return (
+    // Main view container with defined styles
     <View style={styles.container}>
       <View style={styles.contentView}>
+        {/* Title for the settings section */}
         <Text style={styles.title}>Settings</Text>
+        {/* Separator view with specific styles */}
         <View
           style={styles.separator}
           lightColor="#eee"
           darkColor="rgba(255,255,255,0.1)"
         />
+        {/* Display a random quote */}
         <Text style={styles.text}> "{quotes[i]}" - Anonymous </Text>
+        {/* Separator view */}
         <View
           style={styles.separator}
           lightColor="#eee"
           darkColor="rgba(255,255,255,0.1)"
         />
+        {/* Text prompting to select time between actions */}
         <Text style={styles.smallerText}>
           Please Select the Time Between Actions:
         </Text>
+        {/* Display the current slider value */}
         <Text>{Math.round(value * 10) / 10} Seconds</Text>
+        {/* Slider component for selecting time interval */}
         <Slider
           style={{ width: 200, height: 40 }}
           minimumValue={1}
@@ -92,21 +68,23 @@ export default function TabTwoScreen() {
           minimumTrackTintColor="#FFFFFF"
           maximumTrackTintColor="#808080"
         />
+        {/* Separator view */}
         <View
           style={styles.separator}
           lightColor="#eee"
           darkColor="rgba(255,255,255,0.1)"
         />
+        {/* Display an image (logo) */}
         <Image source={logoSource} style={styles.logo} />
       </View>
 
+      {/* Text at the bottom */}
       <View style={styles.bottomText}>
         <Text>Made with Passion - Darren Pinto</Text>
       </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   logo: {
     width: 150, // Adjust the width as needed
